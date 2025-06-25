78617496d5c6a1cc4256a06d7c9dd881fa5a4631
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2025-06-25 15:02:48 +0900

    feat: 환경변수 설정 추가

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './common/config/DatabaseConfig';
@@ -7,6 +8,13 @@ import { OrderModule } from './order/OrderModule';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        `.env.${process.env.NODE_ENV}`, // NODE_ENV 기반 env 파일 우선
+        '.env', // 공통 설정 (fallback)
+      ],
+    }),
     TypeOrmModule.forRoot(typeOrmConfig),
     ScheduleModule.forRoot(),
     MarketDataModule,
